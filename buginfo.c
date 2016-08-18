123aabbe9894bbd5929e5ac960047934cec5564e
rjs <R_js>
2016-08-18 21:50:36 +0800

    finish test

--- buginfo.c
+++
@@ -1,8 +0,0 @@
-list.c: In function ‘MakeEmpty’:
-list.c:17:9: warning: implicit declaration of function ‘FataError’ [-Wimplicit-function-declaration]
-         FataError("Can't create a List");
-         ^
-imp.c: In function ‘main’:
-imp.c:23:12: warning: implicit declaration of function ‘FatalError’ [-Wimplicit-function-declaration]
-            FatalError("OUt of space!");
-            ^
